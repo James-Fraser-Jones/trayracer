cf8dddaa4987693565a64ce7df517911d062979c
smudgecat123 <jf14218@my.bristol.ac.uk>
2021-02-25 14:05:16 +0000

    scaffolding set up for basic window logic

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
-use ash::{vk, Entry, version::EntryV1_0};
+use hello_app::*;
 
-//supposed "hello world" example below but doesn't actually compile
-//nor is most of it used in triangle example in ash_master project
+mod hello_app {
+    pub struct HelloApp();
+    
+    impl HelloApp {
+        pub fn run(&self) {
+            HelloApp::initWindow();
+            HelloApp::initVulcan();
+            HelloApp::mainLoop();
+            HelloApp::cleanup();
+        }
 
-// let entry = Entry::new()?;
-// let app_info = vk::ApplicationInfo {
-//     api_version: vk::make_version(1, 0, 0),
-//     ..Default::default()
-// };
-// let create_info = vk::InstanceCreateInfo {
-//     p_application_info: &app_info,
-//     ..Default::default()
-// };
-// let instance = unsafe { entry.create_instance(&create_info, None)? };
-
-fn main() {
-    println!("Hello, world!");
+        fn initWindow() {
+            /*
+            We're currently working on opening a window here, 
+            having logic in "initVulcan" to keep looping while the window remains open 
+            and then doing cleanup when it gets closed in "cleanup"
+            https://github.com/MaikKlein/ash/blob/master/ash-window/examples/winit.rs
+            https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Base_code
+            */
+        }
+    
+        fn initVulcan() {
+    
+        }
+    
+        fn mainLoop() {
+    
+        }
+    
+        fn cleanup() {
+    
+        }
+    }
 }
 
-//next step: implement rust translation of this (https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Base_code)
+fn main() {
+    let app = HelloApp();
+    app.run(); //should have "try-catch"-like error handling here but probably not necessary in rust???
+    println!("Hello, world!");
+}
