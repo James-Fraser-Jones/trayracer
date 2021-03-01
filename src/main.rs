c0b5ae3e35f44eb199e8b0ab09778bb74ea510ec
smudgecat123 <jf14218@my.bristol.ac.uk>
2021-03-01 14:05:32 +0000

    wasn't able to wrestle winit code into class structure so will do opposite and wrestle vulkan code into winit code structure instead, also successfully added a quit on Esc press

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,71 @@
-use hello_app::*;
-use std::error::Error;
+use winit::{
+    event::{Event, WindowEvent, KeyboardInput, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+    dpi::{Size, PhysicalSize},
+};
 
-mod hello_app {
+const WIDTH : u32 = 800;
+const HEIGHT : u32 = 600;
 
-    use winit::{
-        event::{Event, WindowEvent},
-        event_loop::{ControlFlow, EventLoop},
-        window::{Window, WindowBuilder},
-        dpi::{Size, PhysicalSize},
-    };
-    use std::error::Error;
-
-    const WIDTH : u32 = 800;
-    const HEIGHT : u32 = 600;
-
-    pub struct HelloApp {
-        event_loop : EventLoop<()>,
-        window : Window,
-    }
-    
-    impl HelloApp {
-
-        pub fn new() -> HelloApp<> {
-            let (event_loop, window) = HelloApp::initWindow();
-            HelloApp {
-                event_loop, 
-                window,
-            }
-        }
-
-        fn initWindow() -> (EventLoop<()>, Window) {
-            let event_loop = EventLoop::new();
-            let window = WindowBuilder::new()
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
                 .with_inner_size(Size::from(PhysicalSize::new(WIDTH, HEIGHT)))
                 .with_title("Trayracer")
                 .with_resizable(false)
                 .build(&event_loop)
                 .expect("Issue initializing window"); //causes immediate panic! on failure, this seems reasonable
-            (event_loop, window)
-        }
 
-        pub fn run(&self) {
-            self.initVulcan();
-            self.mainLoop();
-            self.cleanup();
-        }
-        fn initVulcan(&self) {
-            //nothing goes in here yet
-        }
-        fn mainLoop(&self) {
-            //https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Base_code (still stuck at this point, almost finished now)
-            //https://github.com/MaikKlein/ash/blob/master/ash-window/examples/winit.rs (old code example, doesn't seem to work)
-            //https://github.com/rust-windowing/winit (code below does work but complains about lack of static lifetime)
+    event_loop.run(move |event, _, control_flow| {
+        // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+        // dispatched any events. This is ideal for games and similar applications.
+        *control_flow = ControlFlow::Poll;
 
-            // self.event_loop.run(move |event, _, control_flow| {
-            //     *control_flow = ControlFlow::Wait;
-        
-            //     match event {
-            //         Event::WindowEvent {
-            //             event: WindowEvent::CloseRequested,
-            //             window_id,
-            //         } if window_id == self.window.id() => *control_flow = ControlFlow::Exit,
-            //         _ => (),
-            //     }
-            // });
-        }
-        fn cleanup(&self) {
-            //some of code above I think perhaps needs to go in here, not certain though
-        }
-    }
-}
+        // ControlFlow::Wait pauses the event loop if no events are available to process.
+        // This is ideal for non-game applications that only update in response to user
+        // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+        *control_flow = ControlFlow::Wait;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let app = HelloApp::new();
-    app.run();
-    Ok(())
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("Window closed; stopping");
+                *control_flow = ControlFlow::Exit
+            },
+            Event::WindowEvent { //custom escape button handling
+                event: WindowEvent::KeyboardInput {
+                    input: KeyboardInput {
+                        virtual_keycode: Some(VirtualKeyCode::Escape), 
+                        ..
+                    }, 
+                    ..
+                },
+                ..
+            } => {
+                println!("The escape button was pressed; stopping");
+                *control_flow = ControlFlow::Exit
+            },
+            Event::MainEventsCleared => {
+                // Application update code.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw, in
+                // applications which do not always need to. Applications that redraw continuously
+                // can just render here instead.
+                window.request_redraw();
+            },
+            Event::RedrawRequested(_) => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in MainEventsCleared, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+            },
+            _ => ()
+        }
+    });
 }
